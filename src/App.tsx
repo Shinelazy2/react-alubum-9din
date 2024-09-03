5484a6ae1462cce6cb4a98050decc970d9662369
hyunjun Park <shinelazy2@gmail.com>
2024-09-03 15:10:51 +0900

    set router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import MainPage from "@pages/";
+import AboutPage from "@pages/about";
+
 function App() {
-  return <div>App Components Display</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index path="/" element={<MainPage />}></Route>
+        <Route path="/about" element={<AboutPage />}></Route>
+        <Route path="/about/:id" element={<AboutPage />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
